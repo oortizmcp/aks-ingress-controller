c9255c69d700df0a8664bdef937d15034b4c12ea
Omar Ortiz <oortiz.mcp@outlook.com>
2021-09-17 16:58:10 -0500

    Added commands for helm version 3.7.0

--- ingress/configure-ingress-controller.sh
+++ ingress/configure-ingress-controller.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-ingress_name=$1
-acr_url=$2
-namespace=$3
-ingress_values_file=$4
-ingress_manifest_file=$5
+INGRESS_NAME=$1
+ACR_URL=$2
+NAMESPACE=$3
+INGRESS_VALUES_FILE=$4
+AKS_HELLO=$5
+INGRESS_DEMO=$6
+INGRESS_MANIFEST=$7
 
 
 
 #REGISTRY_NAME=<yourregistryname>
-#acr_url=$registry_name + '.azurecr.io'
+#ACR_URL=$registry_name + '.azurecr.io'
 CONTROLLER_REGISTRY=k8s.gcr.io
 CONTROLLER_IMAGE=ingress-nginx
 CONTROLLER_TAG=v0.48.1
@@ -21,24 +23,45 @@ DEFAULTBACKEND_TAG=1.5
 #ACR_URL=<yourregistryname>.azurecr.io
 
 # create namespace if doesn't exists
-kubectl create namespace $namespace --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
-# Install ingress controller if doesn't exists
-helm upgrade --install $ingress_name ingress-nginx/ingress-nginx --version 3.36.0 --namespace $namespace \
+# Authenticate with ACR with --expose-token
+export HELM_EXPERIMENTAL_OCI=1
+TOKEN=$(az acr login --name $ACR_URL --expose-token --output tsv --query accessToken)
+echo $TOKEN | helm registry login $ACR_URL --username 00000000-0000-0000-0000-000000000000 --password-stdin
+
+# Pull Helm Chart from Azure Container Registry & extract files
+helm pull oci://$ACR_URL/<pathofregistry> --version 3.36.0 --untar
+
+#validate chart is desired version
+helm show chart ingress-nginx
+
+# Deploy nginx-ingress chart with helm to AKS Cluster and install ingress controller if doesn't exists
+helm upgrade --install $INGRESS_NAME ingress-nginx --version 3.36.0 --namespace $NAMESPACE \
 --set controller.replicaCount=2 \
 --set controller.nodeSelector."kubernetes\.io/os"=linux \
---set controller.image.registry=$acr_url \
+--set controller.image.registry=$ACR_URL \
 --set controller.image.image=$CONTROLLER_IMAGE \
 --set controller.image.tag=$CONTROLLER_TAG  \
 --set controller.image.digest="" \
 --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux \
---set controller.admissionWebhooks.patch.image.registry=$acr_url \
+--set controller.admissionWebhooks.patch.image.registry=$ACR_URL \
+--set controller.admissionWebhooks.patch.image.digest=""
 --set controller.admissionWebhooks.patch.image.image=$PATCH_IMAGE \
 --set controller.admissionWebhooks.patch.image.tag=$PATCH_TAG \
 --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
---set defaultBackend.image.registry=$acr_url \
+--set defaultBackend.image.registry=$ACR_URL \
 --set defaultBackend.image.image=$DEFAULTBACKEND_IMAGE \
---set defaultBackend.image.tag=$DEFAULTBACKEND_TAG -f $ingress_values_file
+--set defaultBackend.image.tag=$DEFAULTBACKEND_TAG \
+--set defaultBackend.image.digest="" \
+-f $INGRESS_VALUES_FILE
+
+# Create aks-helloworld and ingress-demo
+kubectl apply -f $AKS_HELLO --namespace $NAMESPACE
+kubectl apply -f $INGRESS_DEMO --namespace $NAMESPACE
+
+# Delete validatingwebhookconfigurations
+kubectl delete validatingwebhookconfigurations nginx-ingress-ingress-nginx-admission
 
 # Deploy manifest to AKS
-kubectl apply -f $ingress_manifest_file    
+kubectl apply -f $INGRESS_MANIFEST    
